6d675d969a14862ce7c393bceba3eacaa89f563b
Ishaan <whthownothing@gmail.com>
2022-04-09 20:12:51 +0530

    discard operator working with \s after

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 use bigdecimal::BigDecimal;
+use biiter::BiIterator;
 use logos::{skip, Lexer, Logos};
 use nom::{
     bytes::complete::{tag, take_till1, take_until},
     error::Error as NomError,
 };
 use num_bigint::BigInt;
+use std::convert::From;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{BTreeMap, BTreeSet},
     str::FromStr,
 };
 fn keyword_get(lex: &mut Lexer<Token>) -> Option<Keyword> {
     let output: (&str, &str) =
-        take_till1::<_, &str, NomError<&str>>(|c| c == '/')(lex.slice()).unwrap();
+        take_till1::<_, &str, NomError<&str>>(|c| c == '/')(lex.slice()).expect("Should not occur");
     if output.0.starts_with('/') {
-        let name = tag::<&str, &str, NomError<&str>>("/")(output.0).unwrap().0;
+        let name = tag::<&str, &str, NomError<&str>>("/")(output.0).expect("Should not occur").0;
         if output.1 == "" {
             return Some(Keyword {
                 namespace: None,
@@ -21,12 +23,12 @@ fn keyword_get(lex: &mut Lexer<Token>) -> Option<Keyword> {
             });
         } else {
             return Some(Keyword {
-                namespace: Some(output.1.get(1..).unwrap().to_string()),
+                namespace: Some(output.1.get(1..).expect("Should not occur").to_string()),
                 name: name.to_string(),
             });
         }
     } else {
-        let name = tag::<&str, &str, NomError<&str>>(":")(output.1).unwrap().0;
+        let name = tag::<&str, &str, NomError<&str>>(":")(output.1).expect("Should not occur").0;
         return Some(Keyword {
             namespace: None,
             name: name.to_string(),
@@ -36,13 +38,13 @@ fn keyword_get(lex: &mut Lexer<Token>) -> Option<Keyword> {
 fn bigdecimal_lex(lex: &mut Lexer<Token>) -> BigDecimal {
     let output = tag::<&str, &str, NomError<&str>>("M")(lex.slice());
     match output {
-        Ok(num) => return BigDecimal::from_str(num.0).unwrap(),
-        Err(_) => return BigDecimal::from_str(lex.slice()).unwrap(),
+        Ok(num) => return BigDecimal::from_str(num.0).expect("Should not occur"),
+        Err(_) => return BigDecimal::from_str(lex.slice()).expect("Should not occur"),
     }
 }
 fn symbol_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
     let output: (&str, &str) =
-        take_till1::<_, &str, NomError<&str>>(|c| c == '/')(lex.slice()).unwrap();
+        take_till1::<_, &str, NomError<&str>>(|c| c == '/')(lex.slice()).expect("Should not occur");
     if output.0.starts_with('/') {
         if output.1 == "" {
             return Some(Symbol {
@@ -52,7 +54,7 @@ fn symbol_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
         } else {
             return Some(Symbol {
                 namespace: Some(output.1.to_string()),
-                name: output.0.get(1..).unwrap().to_string(),
+                name: output.0.get(1..).expect("Should not occur").to_string(),
             });
         }
     } else {
@@ -65,10 +67,10 @@ fn symbol_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
 fn tag_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
     let output = take_till1::<_, &str, NomError<&str>>(|c| c == '/')(
         tag::<&str, &str, NomError<&str>>("#")(lex.slice())
-            .unwrap()
+            .expect("Should not occur")
             .0,
     )
-    .unwrap();
+    .expect("Should not occur");
     if output.0.starts_with('/') {
         if output.1 == "" {
             return Some(Symbol {
@@ -78,7 +80,7 @@ fn tag_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
         } else {
             return Some(Symbol {
                 namespace: Some(output.1.to_string()),
-                name: output.0.get(1..).unwrap().to_string(),
+                name: output.0.get(1..).expect("Should not occur").to_string(),
             });
         }
     } else {
@@ -88,12 +90,12 @@ fn tag_get(lex: &mut Lexer<Token>) -> Option<Symbol> {
         });
     }
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
 pub struct Symbol {
     namespace: Option<String>,
     name: String,
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
 pub struct Keyword {
     namespace: Option<String>,
     name: String,
@@ -102,23 +104,23 @@ fn string_callback(lex: &mut Lexer<Token>) -> Option<String> {
     Some(
         take_until::<&str, &str, NomError<&str>>("\"")(
             tag::<&str, &str, NomError<&str>>("\"")(lex.slice())
-                .unwrap()
+                .expect("Should not occur")
                 .0,
         )
-        .unwrap()
+        .expect("Should not occur")
         .1
         .to_string(),
     )
 }
-#[derive(Debug, PartialEq, Logos)]
-pub enum Token {
+#[derive(Debug, PartialEq, Logos, Clone)]
+enum Token {
     #[regex(r":[^\s]+", keyword_get)]
     Keyword(Keyword),
     #[token(r"nil")]
     Nil,
     #[regex(r"\s", skip)]
     Space,
-    #[regex(r#"[^:^\s^#][^\s^"]+"#, symbol_get)]
+    #[regex(r#"[^:^\s^#^;][^\s^"]+"#, symbol_get)]
     Symbol(Symbol),
     #[token(r"\n", skip)]
     NewLine,
@@ -126,6 +128,8 @@ pub enum Token {
     Tag(Symbol),
     #[regex(r"[\t|\s\s\s\s]", skip, priority = 2)]
     Tab,
+    #[regex(r"\\.", callback = |lex| {tag::<&str, &str, NomError<&str>>(r"\")(lex.slice()).unwrap().0.parse::<char>().expect("Should not occur")})]
+    Char(char),
     #[token(r"{")]
     MapStart,
     #[token(r"}")]
@@ -136,22 +140,147 @@ pub enum Token {
     VectorEnd,
     #[regex(r#""[^"]*""#, string_callback, priority = 3)]
     Str(String),
-    #[regex(r";\s?[^\n]*", skip, priority = 1)]
+    #[regex(r";\s?[^\n]*", skip, priority = 2)]
     Comment,
     #[token(r"#{")]
     SetStart,
-    #[regex(r"\d+", |lex| BigInt::from_str(lex.slice()).unwrap(), priority = 3)]
+    #[regex(r"\d+", |lex| BigInt::from_str(lex.slice()).expect("Should not occur"), priority = 3)]
     BigInt(BigInt),
     #[regex(r"M?\d+\.\d+", bigdecimal_lex)]
     BigDec(BigDecimal),
-    #[regex(r"#_\s?[^\n]+", skip)]
-    Discard,
     #[error]
     Error,
 }
-#[inline(always)]
-pub fn lex_str(input: &str) -> Vec<Token> {
-    Token::lexer(input).collect()
+#[derive(Debug, Clone)]
+pub enum Value {
+    Nil,
+    Boolean(bool),
+    Character(char),
+    String(String),
+    Symbol(Symbol),
+    Keyword(Keyword),
+    BigInt(BigInt),
+    BigDec(BigDecimal),
+    Vector(Vec<Value>),
+    Map(BTreeMap<Value, Value>),
+    TaggedElement(Symbol, Box<Value>),
+    Set(BTreeSet<Value>),
+}
+fn equal(v1: &Value, v2: &Value) -> bool {
+    match (v1, v2) {
+        (Value::Nil, Value::Nil) => true,
+        (Value::Boolean(b1), Value::Boolean(b2)) => b1 == b2,
+        (Value::String(s1), Value::String(s2)) => s1 == s2,
+        (Value::Character(c1), Value::Character(c2)) => c1 == c2,
+        (Value::Symbol(s1), Value::Symbol(s2)) => s1 == s2,
+        (Value::Keyword(k1), Value::Keyword(k2)) => k1 == k2,
+        (Value::BigInt(bi1), Value::BigInt(bi2)) => bi1 == bi2,
+        (Value::BigDec(bd1), Value::BigDec(bd2)) => bd1 == bd2,
+        (Value::Vector(vals1), Value::Vector(vals2)) => vals1 == vals2,
+        (Value::TaggedElement(tag1, value1), Value::TaggedElement(tag2, value2)) => {
+            equal(&Value::Symbol(tag1.clone()), &Value::Symbol(tag2.clone()))
+                && equal(value1, value2)
+        }
+        _ => false,
+    }
+}
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        equal(self, other)
+    }
+}
+impl Value {
+    fn parse_tokens(input: Vec<Token>) -> Vec<Value> {
+        let mut values = Vec::new();
+        let mut iter: BiIterator<Token> =
+            BiIterator::from(input.iter().cloned().collect::<Vec<Token>>());
+        while let Some(i) = iter.next() {
+            match i {
+                Token::Keyword(key) => values.push(Value::Keyword(key)),
+                Token::Nil => values.push(Value::Nil),
+                Token::Space => {}
+                Token::Symbol(symbol) => values.push(Value::Symbol(symbol)),
+                Token::NewLine => {}
+                Token::Tag(symbol) => values.push(Value::TaggedElement(
+                    symbol,
+                    Box::new(Value::new(
+                        iter.peek().expect("Must have element after tag").to_owned(),
+                    )),
+                )),
+                Token::Tab => {}
+                Token::Char(c) => values.push(Value::Character(c)),
+                Token::MapStart => todo!(),
+                Token::SetMapEnd => todo!(),
+                Token::VectorStart => todo!(),
+                Token::VectorEnd => todo!(),
+                Token::Str(str) => values.push(Value::String(str)),
+                Token::Comment => {}
+                Token::SetStart => todo!(),
+                Token::BigInt(int) => values.push(Value::BigInt(int)),
+                Token::BigDec(dec) => values.push(Value::BigDec(dec)),
+                Token::Error => {}
+            }
+        }
+        values
+    }
+    fn new(token: Token) -> Value {
+        todo!()
+    }
+}
+fn lex_str(input: &str) -> Vec<Token> {
+    let mut words: Vec<&str> = input.split(" ").collect();
+    let mut pars = 0usize;
+    while let Some(i) = words.iter().position(|x| *x == "#_" || x.starts_with("#_")) {
+        if i != words.len() {
+            if &"#{" == words.get(i + 1).expect("Should not occur")
+                || &"{" == words.get(i + 1).expect("Should not occur")
+                || &"[" == words.get(i + 1).expect("Should not occur")
+            {
+                pars += 1;
+                let mut buffer = Vec::new();
+                for (j, _) in words.clone().iter().enumerate() {
+                    if j == i {
+                        pars -= 1;
+                    }
+                    if &"#{" == words.get(j + 1).unwrap_or(&"0")
+                        || &"{" == words.get(j + 1).unwrap_or(&"0")
+                        || &"[" == words.get(j + 1).unwrap_or(&"0")
+                    {
+                        pars += 1;
+                    } else if &"}" == words.get(i + 1).expect("Should not occur")
+                        || &"]" == words.get(i + 1).expect("Should not occur")
+                        || &"}" == words.get(i).expect("Should not occur")
+                        || &"]" == words.get(i).expect("Should not occur")
+                    {
+                        pars -= 1;
+                    }
+                    buffer.push(j);
+                    if pars == 0 {
+                        break;
+                    }
+                }
+                pars -= 1;
+                if pars != 0 {
+                    panic!("Unmatched parentheses")
+                }
+                for j in buffer.iter().cloned() {
+                    if j < words.len() - 1 {
+                        words.remove(j);
+                    } else if j == words.len() {
+                        words.remove(j - 1);
+                    }
+                }
+            };
+        }
+        words.remove(i + 1);
+        words.remove(i);
+    }
+    let mut input = String::new();
+    for i in words.iter() {
+        input.push_str(i);
+        input.push(' ')
+    }
+    Token::lexer(input.trim()).collect()
 }
 
 #[cfg(test)]
@@ -255,7 +384,7 @@ mod tests {
         let mut lexer = Token::lexer("12");
         assert_eq!(
             lexer.next(),
-            Some(Token::BigInt(BigInt::from_str("12").unwrap()))
+            Some(Token::BigInt(BigInt::from_str("12").expect("Should not occur")))
         )
     }
     #[test]
@@ -263,12 +392,12 @@ mod tests {
         let mut lexer = Token::lexer("12.3");
         assert_eq!(
             lexer.next(),
-            Some(Token::BigDec(BigDecimal::from_str("12.3").unwrap()))
+            Some(Token::BigDec(BigDecimal::from_str("12.3").expect("Should not occur")))
         );
         let mut lexer = Token::lexer("M12.3");
         assert_eq!(
             lexer.next(),
-            Some(Token::BigDec(BigDecimal::from_str("12.3").unwrap()))
+            Some(Token::BigDec(BigDecimal::from_str("12.3").expect("Should not occur")))
         );
     }
     #[test]
@@ -284,7 +413,13 @@ mod tests {
     }
     #[test]
     fn discard_tests() {
-        let mut lexer = Token::lexer("#_Hello");
-        assert_eq!(lexer.next(), None)
+        let tokens = lex_str("#_ He \"llo\"");
+        assert_eq!(tokens, vec![Token::Str("llo".to_string())]);
+        let tokens = lex_str("#_ [ 1 2 3 ]");
+        assert_eq!(tokens, vec![]);
+        let tokens = lex_str("#_He \"llo\"");
+        assert_eq!(tokens, vec![Token::Str("llo".to_string())]);
+        let tokens = lex_str("#_[ 1 2 3 ]");
+        assert_eq!(tokens, vec![]);
     }
 }
